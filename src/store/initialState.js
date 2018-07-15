85aa141603297e7f6b08d73be2245845e7c4d570
anthonyDrury <anthony.drury1@gmail.com>
2018-07-15 19:17:35 +1000

    Updated upgrades for sales

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -112,6 +112,96 @@ const initialState = {
       amount: 0,
       target: ["anchovie"],
       disabled: "disabled"
+    },
+    {
+      id: 1,
+      title: "Fish Biker",
+      price: 1000,
+      initialPrice: 1000,
+      perSecondBonus: 10,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 2,
+      title: "Fish Cart",
+      price: 10000,
+      initialPrice: 10000,
+      perSecondBonus: 100,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 3,
+      title: "Entrepreneur",
+      price: 100000,
+      initialPrice: 100000,
+      perSecondBonus: 500,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 4,
+      title: "Fish'n'Shop",
+      price: 500000,
+      initialPrice: 500000,
+      perSecondBonus: 1000,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 5,
+      title: "Fish R Us",
+      price: 1000000,
+      initialPrice: 1000000,
+      perSecondBonus: 2000,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 6,
+      title: "Fish 2 U",
+      price: 10000000,
+      initialPrice: 10000000,
+      perSecondBonus: 5000,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 7,
+      title: "Fish Coin",
+      price: 15000000,
+      initialPrice: 15000000,
+      perSecondBonus: 8000,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 7,
+      title: "Fish Pill",
+      price: 35000000,
+      initialPrice: 35000000,
+      perSecondBonus: 10000,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
+    },
+    {
+      id: 8,
+      title: "~Fysh~",
+      price: 100000000,
+      initialPrice: 100000000,
+      perSecondBonus: 100000,
+      amount: 0,
+      target: ["anchovie"],
+      disabled: "disabled"
     }
   ]
 };
